6c27156b94550829d939a9661b77336cd459209f
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-06 17:11:56 -0700

    almost done but buttons not working

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,36 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+import { addToMemory, applyNumber, changeOperation, clearDisplay, clearMemory, memoryToTotal } from '../actions';
 
 function App() {
   
   const [state, dispatch] = useReducer(reducer, initialState);
-  
+  const handleNum = (value) => {
+    dispatch(applyNumber(value));
+  }
 
+  const handleOper = (value) => {
+    dispatch(changeOperation(value))
+  }
+  
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay());
+  }
+
+  const handleAddToMemory = () => {
+    dispatch(addToMemory());
+  }
+
+  const handleMemoryToTotal = () => {
+    dispatch(memoryToTotal());
+  }
+
+  const handleClearMemory = () => {
+    dispatch(clearMemory());
+  }
+
+    
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -25,42 +49,43 @@ function App() {
             
             <TotalDisplay value={0}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> Operation</span>
+              <span id="memory"><b>Memory:</b> Memory</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick={() => handleAddToMemory()}  value={"M+"}/>
+              <CalcButton onClick={() => handleMemoryToTotal()} value={"MR"}/>
+              <CalcButton onClick={() => handleClearMemory()}  value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+            <CalcButton onClick={() => handleNum(1)} value={1}/>
+              <CalcButton onClick={() => handleNum(2)} value={2}/>
+              <CalcButton onClick={() => handleNumr(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+            <CalcButton onClick={() => handleNum(4)} value={4}/>
+              <CalcButton onClick={() => handleNum(5)} value={5}/>
+              <CalcButton onClick={() => handleNum(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+            <CalcButton onClick={() => handleNum(7)} value={7}/>
+              <CalcButton onClick={() => handleNum(8)} value={8}/>
+              <CalcButton onClick={() => handleNum(9)} value={9}/>
+
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+            <CalcButton onClick={() => handleOper("+")} value={"+"}/>
+              <CalcButton onClick={() => handleOper("*")} value={"*"}/>
+              <CalcButton onClick={() => handleOper("-")} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton  onClick={handleClearDisplay} value={"CE"}/>
             </div>
 
           </form>
